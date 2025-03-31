d592892345a405e1ff56ec6a06534a92cbbfea85
omilli <oli.millington@gmail.com>
2025-03-31 20:36:42 +0700

    just render element in render, don't return

--- lib/render/api.ts
+++ lib/render/api.ts
@@ -5,41 +5,32 @@ import { renderDomElement } from "./dom";
 import { renderStringElement } from "./string";
 
 /**
- * Renders a hypertext node (VNode) as either a DOM element or a string.
+ * Renders a VNode as either a DOM element or a string.
  *
  * This function handles rendering in both client and server environments:
  * - In server environments (where `window` is undefined), it renders the VNode as a string.
  * - In client environments, it renders the VNode to the DOM at the specified root selector.
  *
- * @param vNode - The hypertext node to render
+ * @param vNode - The node to render
  * @param rootSelector - Optional CSS selector to target the DOM element where rendering should occur
  * @param context - The rendering context, defaults to value from getDefaultContext()
  *
  * @returns In server environments, returns a string representation of the rendered element.
- *          In client environments, returns a {@link RenderedNode} object containing the rendered DOM element and original vNode.
+ *          In client environments, returns a RenderedNode object containing the rendered DOM element and original vNode.
  */
 export function render(
 	vNode: VNode,
 	rootSelector?: string,
 	context = getDefaultContext(),
-): string | RenderedNode {
-	// Server environment
+) {
+	// If we're in a server environment
 	if (typeof window === "undefined") {
+		// render a String
 		return renderStringElement(vNode);
 	}
-
-	// Client environment
+	// We're in a browser from this point
+	// Get the root element
 	const rootElement = getRootElement(rootSelector);
-
-	const element = renderDomElement(
-		vNode,
-		rootElement,
-		rootSelector as string,
-		context,
-	);
-
-	return {
-		element,
-		vNode,
-	};
+	// Render the element to the DOM
+	renderDomElement(vNode, rootElement, rootSelector as string, context);
 }
