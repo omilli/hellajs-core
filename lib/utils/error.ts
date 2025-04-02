a6b65afb7bde355beb30cb91535be735dccd88fe
omilli <oli.millington@gmail.com>
2025-04-02 08:22:58 +0700

    error docs

--- lib/utils/error.ts
+++ lib/utils/error.ts
@@ -1,3 +1,12 @@
+/**
+ * Handles errors by either invoking a provided callback or throwing a formatted error message.
+ * 
+ * @param error - The error to be handled. Can be of any type.
+ * @param onError - Optional callback function that will be invoked if the error is an instance of Error.
+ *                  If provided and the error is an Error instance, this callback will be called instead of throwing.
+ * 
+ * @throws Throws a string with error information if no callback is provided or if the error is not an Error instance.
+ */
 export function handleError(error: unknown, onError?: (error: Error) => void) {
 	if (onError && error instanceof Error) {
 		onError(error);
